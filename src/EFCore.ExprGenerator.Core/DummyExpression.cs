9e89f017d26d1dd44198f8711dcd3d4c90093e2c
Arika Ishinami <delete0093@gmail.com>
2025-11-11 07:07:34 +0000

    fix: add OverloadResolutionPriority attribute to SelectExpr method

--- src/EFCore.ExprGenerator.Core/DummyExpression.cs
+++ src/EFCore.ExprGenerator.Core/DummyExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 /// <summary>
 /// Dummy expression methods for EFCore.ExprGenerator to compile correctly.
@@ -9,6 +10,7 @@ public static class DummyExpression
     /// <summary>
     /// Dummy Select expression method for EFCore.ExprGenerator to compile correctly.
     /// </summary>
+    [OverloadResolutionPriority(-1)]
     public static IQueryable<TResult> SelectExpr<T, TResult>(
         this IQueryable<T> query,
         Func<T, TResult> selector
