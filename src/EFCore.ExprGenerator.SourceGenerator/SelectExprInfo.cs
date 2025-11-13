c8d2ae945d2a0c7fd855cf196e2e3141e2e4cef5
Arika Ishinami <delete0093@gmail.com>
2025-11-13 04:41:30 +0000

    refactor: remove redundant default value cases for numeric types in GetDefaultValueForType method

--- src/EFCore.ExprGenerator.SourceGenerator/SelectExprInfo.cs
+++ src/EFCore.ExprGenerator.SourceGenerator/SelectExprInfo.cs
@@ -303,17 +303,6 @@ internal abstract record SelectExprInfo
             SpecialType.System_Boolean => "false",
             SpecialType.System_Char => "'\\0'",
             SpecialType.System_String => "string.Empty",
-            SpecialType.System_SByte
-            or SpecialType.System_Byte
-            or SpecialType.System_Int16
-            or SpecialType.System_UInt16
-            or SpecialType.System_Int32
-            or SpecialType.System_UInt32
-            or SpecialType.System_Int64
-            or SpecialType.System_UInt64
-            or SpecialType.System_Single
-            or SpecialType.System_Double
-            or SpecialType.System_Decimal => "0",
             _ => "default",
         };
     }
