d65e6b3ac7c9bb31c4a75f3513be4f6cdbffb484
Arika Ishinami <delete0093@gmail.com>
2025-11-16 00:29:42 +0900

    Version 0.2.3
    feat: add Linqraft.ApiSample project with initial setup and sample API implementation
    
    (cherry picked from commit c872387c04dbf9c76936ae49126a68f1e41d9f0e)
    
    feat: add GetDtoNamespace method to SelectExprInfo classes for improved namespace handling
    
    close #23 #24
    
    (cherry picked from commit 5311e3d5e769738382cf390566f9e5a385eb7f0b)

---
+++ examples/Linqraft.ApiSample/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linqraft.ApiSample;
+
+[Route("api/[controller]")]
+[ApiController]
+public partial class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<OrderDto>> Get()
+    {
+        var Orders = new List<Order>();
+        return Orders
+            .AsQueryable()
+            .SelectExpr<Order, OrderDto>(s => new
+            {
+                Id = s.Id,
+                CustomerName = s.Customer?.Name,
+                CustomerCountry = s.Customer?.Address?.Country?.Name,
+                CustomerCity = s.Customer?.Address?.City?.Name,
+                Items = s
+                    .OrderItems.Select(oi => new
+                    {
+                        ProductName = oi.Product?.Name,
+                        Quantity = oi.Quantity,
+                    })
+                    .ToList(),
+            })
+            .ToList();
+    }
+}
